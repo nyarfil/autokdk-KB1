62c39818567bf39c825fae996c6864b7dffc1f63
にゃあふぃる <70083938+nyarfil@users.noreply.github.com>
2025-08-29 19:32:30 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/nyarfil_01/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xa1, 0x1a, 0xd0, 0x9a, 0x2a, 0x84, 0x00, 0x56}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
